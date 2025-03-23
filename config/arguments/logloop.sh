8e9f9fe81d95735b69af8026a6c7bddb1f858007
Prashant Singh Negi <prashantsinghnegi7@gmail.com>
2025-03-23 23:10:32 +0530

    Add logloop.sh

---
+++ config/arguments/logloop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+trap "exit" SIGINT
+INTERVAL=$1
+echo "Configured to log messages every $INTERVAL seconds"
+mkdir -p /tmp/logs
+while :
+do
+  echo "$(date) Writing log message to /tmp/logs/log.txt"
+  echo "$(date) Sample log message" > /tmp/logs/log.txt
+  sleep $INTERVAL
+done
